b373d087580c95879b3e71166db4b3a4d9ff39f3
pankaj-pkj <pourwalpkj@gmail.com>
2026-05-13 17:20:38 +0530

    Handle KeyboardInterrupt during asyncio execution

--- main.py
+++ main.py
@@ -205,4 +205,7 @@ async def main():
 if __name__ == "__main__":
     import nest_asyncio
     nest_asyncio.apply()
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
